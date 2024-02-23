b16f4e4c502f3143dde94d966ce1ebfba9630b35
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2024-02-23 23:28:25 +0530

    Update instructor.sql

--- assignment-1/instructor.sql
+++ assignment-1/instructor.sql
@@ -1,13 +1,13 @@
 create table Instructor(ID numeric(5), name varchar(30), dept_name varchar(20), salary numeric(6));
-insert into Instructor values(10101, "Srinivasam", "Comp. Sci.", 65000);
-insert into Instructor values(12121, "Wu", "Finance", 90000);
-insert into Instructor values(15151, "Mozart", "Music", 40000);
-insert into Instructor values(22222, "Einstein", "Physics", 95000);
-insert into Instructor values(32343, "El Said", "History", 60000);
-insert into Instructor values(33456, "Gold", "Physics", 87000);
-insert into Instructor values(45655, "Katz", "Comp. Sci.", 75000);
-insert into Instructor values(58583, "califieri", "History", 62000);
-insert into Instructor values(76543, "Singh", "Finance", 80000);
-insert into Instructor values(76766, "Crick", "Biology", 72000);
-insert into Instructor values(83821, "Brandt", "Comp. Sci.", 92000);
-insert into Instructor values(98345, "Kim", "Elec. Eng.", 80000);
+insert into Instructor values
+	(10101, "Srinivasam", "Comp. Sci.", 65000),
+	(12121, "Wu", "Finance", 90000),
+	(15151, "Mozart", "Music", 40000),
+	(22222, "Einstein", "Physics", 95000),
+	(32343, "El Said", "History", 60000),
+	(33456, "Gold", "Physics", 87000),
+	(45655, "Katz", "Comp. Sci.", 75000),
+	(58583, "califieri", "History", 62000),
+	(76543, "Singh", "Finance", 80000),
+	(76766, "Crick", "Biology", 72000),
+	(83821, "Brandt", "Comp. Sci.", 92000),
