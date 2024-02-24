7a81e7d5d426f73efbef47c5a45c390ee71eaa8f
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2024-02-24 13:09:57 +0530

    Update department.sql

--- assignment-1/department.sql
+++ assignment-1/department.sql
@@ -1,9 +1,9 @@
 create table Department(Dept_name varchar(20), Building varchar(20), Budget numeric(10));
-insert into Department values
-	("Biology", "Watson", 90000),
-	("Comp. Sci.", "Taylor", 100000),
-	("Elec. Eng.", "Taylor", 85000),
-	("Finance", "Painter", 120000),
-	("History", "Painter", 50000),
-	("Music", "Packard", 80000),
-	("Physics", "Watson", 70000);
+
+insert into Department values('Biology', 'Watson', 90000);
+insert into Department values('Comp. Sci.', 'Taylor', 100000);
+insert into Department values('Elec. Eng.', 'Taylor', 85000);
+insert into Department values('Finance', 'Painter', 120000);
+insert into Department values('History', 'Painter', 50000);
+insert into Department values('Music', 'Packard', 80000);
+insert into Department values('Physics', 'Watson', 70000);
