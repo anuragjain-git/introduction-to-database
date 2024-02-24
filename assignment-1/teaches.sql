cbcbe05f87fafcd86348aa0f607aa572bc66943a
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2024-02-24 13:12:15 +0530

    Update teaches.sql

--- assignment-1/teaches.sql
+++ assignment-1/teaches.sql
@@ -1,17 +1,17 @@
 create table Teaches(ID numeric(5), course_id varchar(10), sec_id numeric(2), semester varchar(10), year numeric(4));
-insert into Teaches values
-  (10101, "CS-101", 1, "Fall", 2009),
-  (10101, "CS-315", 1, "Spring", 2010),
-  (10101, "CS-347", 1, "Fall", 2009),
-  (12121, "FIN-201", 1, "Spring", 2010),
-  (15151, "MU-199", 1, "Spring", 2010),
-  (22222, "PHY-101", 1, "Fall", 2009),
-  (32343, "HIS-351", 1, "Spring", 2010),
-  (45565, "CS-101", 1, "Spring", 2010),
-  (45565, "CS-319", 1, "Spring", 2010),
-  (76766, "BIO-101", 1, "Summer", 2009),
-  (76766, "BIO-301", 1, "Summer", 2010),
-  (83821, "CS-190", 1, "Spring", 2009),
-  (83821, "CS-190", 2, "Spring", 2009),
-  (83821, "CS-319", 2, "Spring", 2010),
-  (98345, "EE-181", 1, "Spring", 2009);
+
+insert into Teaches values(10101, 'CS-101', 1, 'Fall', 2009);
+insert into Teaches values(10101, 'CS-315', 1, 'Spring', 2010);
+insert into Teaches values(10101, 'CS-347', 1, 'Fall', 2009);
+insert into Teaches values(12121, 'FIN-201', 1, 'Spring', 2010);
+insert into Teaches values(15151, 'MU-199', 1, 'Spring', 2010);
+insert into Teaches values(22222, 'PHY-101', 1, 'Fall', 2009);
+insert into Teaches values(32343, 'HIS-351', 1, 'Spring', 2010);
+insert into Teaches values(45565, 'CS-101', 1, 'Spring', 2010);
+insert into Teaches values(45565, 'CS-319', 1, 'Spring', 2010);
+insert into Teaches values(76766, 'BIO-101', 1, 'Summer', 2009);
+insert into Teaches values(76766, 'BIO-301', 1, 'Summer', 2010);
+insert into Teaches values(83821, 'CS-190', 1, 'Spring', 2009);
+insert into Teaches values(83821, 'CS-190', 2, 'Spring', 2009);
+insert into Teaches values(83821, 'CS-319', 2, 'Spring', 2010);
+insert into Teaches values(98345, 'EE-181', 1, 'Spring', 2009);
