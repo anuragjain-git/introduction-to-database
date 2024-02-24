90e48038576f8ad2da6c614aa2e883ab1b5b2a8d
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2024-02-24 13:07:45 +0530

    Update prereq.sql

--- assignment-1/prereq.sql
+++ assignment-1/prereq.sql
@@ -1,9 +1,8 @@
 create table Prereq(Course_id varchar(10), Prereq_id varchar(10));
-insert into Prereq values
-	("BIO-301", "BIO-101"),
-	("BIO-399", "BIO-101"),
-	("CS-190", "CS-101"),
-	("CS-315", "CS-101"),
-	("CS-319", "CS-101"),
-	("CS-347", "CS-101"),
-	("EE-181", "PHY-101");
+insert into Prereq values('BIO-301', 'BIO-101');
+insert into Prereq values('BIO-399', 'BIO-101');
+insert into Prereq values('CS-190', 'CS-101');
+insert into Prereq values('CS-315', 'CS-101');
+insert into Prereq values('CS-319', 'CS-101');
+insert into Prereq values('CS-347', 'CS-101');
+insert into Prereq values('EE-181', 'PHY-101');
